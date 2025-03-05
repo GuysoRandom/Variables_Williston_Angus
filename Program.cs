9e57fc12b974823ec6c3854a552e0dd07ffc5f4f
GuysoRandom <angus.r.williston05@gmail.com>
2025-03-05 15:01:32 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Variables_Williston_Angus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
