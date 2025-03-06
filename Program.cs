797ae181f23395a86ec140fe0dfd33cd023963a1
GuysoRandom <angus.r.williston05@gmail.com>
2025-03-06 12:54:10 -0600

    Did it.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Declares an integer to store a favorite number
+            int favoriteNumber;
+            // Booleans initializes Jumping and Running to false
+            bool isJumping = false, isRunning = false;
+            // Declared random float variable
+            double myDouble;
+            // Assigns the favoriteNumber to 777
+            favoriteNumber = 777;
+            // Assigns myDouble to 999.9
+            myDouble = 999.9;
+            // Initializes constant finalGrade to 100.0
+            const double finalGrade = 100.0;
+            
+            //Prints all variable to console
+            Console.WriteLine("Favoriten #: " + favoriteNumber);
+            Console.WriteLine("Jumping: " + isJumping);
+            Console.WriteLine("Running: " + isRunning);
+            Console.WriteLine("Random Floating #: " + myDouble);
+            Console.WriteLine("Final Grade: " + finalGrade);
         }
     }
 }
